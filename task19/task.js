2824b52fe45fd04f59ed37c5215b253390258620
Wee <27firework@gmail.com>
2017-04-13 22:26:06 +0800

    finish task19, but the bubble sort seems wrong…

---
+++ task19/task.js
@@ -0,0 +1,158 @@
+var gVars = {
+  // var leftIn = 
+  deque: {
+    data: [],
+    rightPush: function (value) {
+      this.data.push(value);
+      render();
+    },
+
+    leftPush: function (value) {
+      this.data.unshift(value);
+      render();
+    },
+
+    leftPop: function () {
+      render(this.data.shift());
+    },
+
+    rightPop: function () {
+      render(this.data.pop());
+    },
+
+    deleteIndex: function (index) {
+      render(this.data.splice(index, 1));
+    }
+  },
+  input: document.getElementById("input"),
+  leftIn: document.getElementById("leftIn"),
+  leftOut: document.getElementById("leftOut"),
+  rightIn: document.getElementById("rightIn"),
+  rightOut: document.getElementById("rightOut"),
+  wrapperEle: document.getElementById("wrapper"),
+  random: document.getElementById("random"),
+  bubble: document.getElementById("bubble")
+}
+
+function inputValid(input) {
+  if (!/^\d+$/.test(gVars.input.value)) {
+    return false;
+  }
+  if (gVars.input.value < 10 || gVars.input.value > 100) {
+    return false;
+  }
+  if (gVars.deque.data.length > 60) {
+    return false;
+  }
+  return true;
+}
+
+function render(popValue) {
+  //render
+  gVars.wrapperEle.innerHTML = "";
+  gVars.deque.data.forEach(function (value) {
+    var ele = document.createElement("div");
+    ele.style.height = value * 2 + "px";
+    ele.appendChild(document.createTextNode(value));
+    gVars.wrapperEle.appendChild(ele);
+  });
+
+  //alert
+  if (popValue) {
+    alert(popValue);
+  } else {
+    //clear input
+    gVars.input.value = "";
+  }
+  initClickDel();
+}
+
+//set click del
+function initClickDel() {
+  for (var i = 0; i < gVars.wrapperEle.childElementCount; i++) {
+    //good
+    gVars.wrapperEle.childNodes[i].onclick = function (index) {
+      return function () {
+        gVars.deque.deleteIndex(index);
+      }
+    }(i);
+    //good
+  }
+}
+
+//click listener
+gVars.rightIn.onclick = function () {
+  inputValid() && gVars.deque.rightPush(parseInt(gVars.input.value));
+}
+
+gVars.leftIn.onclick = function () {
+  inputValid() && gVars.deque.leftPush(parseInt(gVars.input.value));
+}
+
+gVars.leftOut.onclick = function () {
+  inputValid() && gVars.deque.leftPop();
+}
+
+gVars.rightOut.onclick = function () {
+  inputValid() && gVars.deque.rightPop();
+}
+
+gVars.rightOut.onclick = function () {
+  inputValid() && gVars.deque.rightPop();
+}
+
+gVars.random.onclick = function () {
+  gVars.deque.data = generateRandomNum();
+  render();
+}
+
+gVars.bubble.onclick = function () {
+  var flag = bubbleSort(gVars.deque.data);
+}
+
+//swap two num
+function swapNode(index1, index2) {
+  var ele1 = gVars.wrapperEle.childNodes[index1];
+  var ele2 = gVars.wrapperEle.childNodes[index2];
+  var tempNode = ele2.cloneNode(true);
+  ele1.parentNode.insertBefore(tempNode, ele1);
+  ele1.parentNode.replaceChild(ele1, ele2);
+  ele1 = null;
+}
+
+//randomize
+function generateRandomNum() {
+  var tempArr = [];
+  for (var i = 0; i < 20 + Math.floor(Math.random() * 20); i++) {
+    tempArr.push(Math.floor(Math.random() * 101));
+  }
+  return tempArr;
+}
+
+//bubble sort
+function bubbleSort(arr) {
+  var i = 0,
+    j = i + 1;
+  var timer = setInterval(run, 10);
+
+  function run() {
+    if (i < arr.length) {
+      if (j < arr.length) {
+        console.log("render");
+        if (arr[j] < arr[i]) {
+          var temp = arr[j];
+          arr[j] = arr[i];
+          arr[i] = temp;
+          // render();
+          swapNode(i, j);
+        }
+        j++;
+      } else {
+        i++;
+        j = i + 1;
+      }
+    } else {
+      clearInterval(timer);
+    }
+  }
+}
